6a6173dba23f90d639768ef948e9672f2a080079
MarconyClavijo <marconyclavijo@hotmail.com>
2026-06-21 02:28:51 -0500

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,9 @@
-const CACHE_NAME = "marcony-radio-v1";
+const CACHE = "marcony-v1";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-// INSTALAR CACHE
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", e=>{
+self.skipWaiting();
 });
 
-// ACTIVAR CACHE
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
-// FETCH (modo offline básico)
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", e=>{
+e.respondWith(fetch(e.request).catch(()=>fetch(e.request)));
 });

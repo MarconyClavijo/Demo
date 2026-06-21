1735afa1bda29e737af2784c02f1aebf3a6b1601
MarconyClavijo <marconyclavijo@hotmail.com>
2026-06-21 00:19:26 -0500

    Crear sw.js
    crear service worker PWA

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "marcony-radio-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// INSTALAR CACHE
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ACTIVAR CACHE
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH (modo offline básico)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
